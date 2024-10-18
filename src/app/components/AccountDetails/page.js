10445b1c6bb6d6e9e4c3f3c93d6454d07581c0da
Dangoo2002 <kennydangote893@gmail.com>
2024-10-18 14:46:34 +0300

    added the account details page

---
+++ src/app/components/AccountDetails/page.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const AccountDetails = () => {
+  return (
+    <div>
+      {/* Content goes here */}
+      <h1>Account Details</h1>
+      <p>Full name: [Full Name]</p>
+      <p>Email: [Email]</p>
+      {/* More details can be added here */}
+    </div>
+  );
+};
+
+export default AccountDetails;
