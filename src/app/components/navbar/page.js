30b6e17dcced362417661459db62769dc820e3e3
Dangoo2002 <kennydangote893@gmail.com>
2025-05-23 02:52:06 +0300

    added

--- src/app/components/navbar/page.js
+++ src/app/components/navbar/page.js
@@ -229,7 +229,7 @@ export default function SocialMediaNav() {
             animate={{ scaleX: 1, opacity: 1 }}
             exit={{ scaleX: 0, opacity: 0 }}
             transition={{ duration: 0.3 }}
-            className="md:hidden px-4 py-2 bg-white/80 dark:bg-gray-900/80 border-t border-gray-300 dark:border-gray-600"
+            className="md:hidden px-4 py-2 bg-white dark:bg-gray-900 border-t border-gray-300 dark:border-gray-600"
           >
             <div className="relative">
               <MagnifyingGlassIcon className="absolute left-3 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-400" />
