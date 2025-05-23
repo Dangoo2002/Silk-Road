17f20baca217abb8c6a865ba04a939aac1e0f915
Dangoo2002 <kennydangote893@gmail.com>
2025-05-23 03:19:59 +0300

    added

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
 import { AuthProvider } from "./components/AuthContext/AuthContext";
-import NavigationLoader from './components/navigationEvents'; // Ensure the path is correct
+
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
